12b667fadd460c8a6124c0e5b9f430ebd046d9d5
ArunTeltia <teltia.arun18@gmail.com>
2020-12-27 21:31:10 +0530

    ads added + sponser name added

--- src/components/device/CardBuilds.vue
+++ src/components/device/CardBuilds.vue
@@ -1,102 +1,129 @@
 <template>
-  <div class="row center">
-    <div class="col s12 m12">
-      <ul class="collapsible collapsible-builds">
-        <li
-          v-for="build in deviceBuilds"
-          :key="build.id"
-          @click="setBuild(build.filename)"
-          class="buildcoll"
-        >
-          <div class="collapsible-header white-text cardColor">
-            <i class="material-icons">system_update</i>
-            <span style="width: 100%">{{ build.filename }}</span>
-            <i class="material-icons">arrow_drop_down</i>
-          </div>
-
-          <div class="collapsible-body">
-            <p class="label">details</p>
-            <div class="buildinfo">
-              <div class="deviceprop">
-                <p>Filename: {{build.filename}}</p>
-              </div>
-              <div class="deviceprop">
-                <p>Date: {{build.datetime}}</p>
-              </div>
-              <div class="deviceprop">
-                <p>Size: {{build.size}}</p>
-              </div>
-              <div class="deviceprop">
-                <p>MD5: {{build.md5}}</p>
-              </div>
-              <div class="deviceprop">
-                <p>Version: {{build.version}}</p>
-              </div>
-              <div class="deviceprop">
-                <p>Downloads: {{build.downloads}}</p>
-              </div>
+  <div>
+    <div class="container" style="width:100%">
+      <center>
+        <script
+          type="application/javascript"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+        <!-- sakura-horizontal-unit-2 -->
+        <ins
+          class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-1277633863266032"
+          data-ad-slot="8931146882"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script type="application/javascript">
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </center>
+    </div>
+    <div class="row center">
+      <div class="col s12 m12">
+        <ul class="collapsible collapsible-builds">
+          <li
+            v-for="build in deviceBuilds"
+            :key="build.id"
+            @click="setBuild(build.filename)"
+            class="buildcoll"
+          >
+            <div class="collapsible-header white-text cardColor">
+              <i class="material-icons">system_update</i>
+              <span style="width: 100%">{{ build.filename }}</span>
+              <i class="material-icons">arrow_drop_down</i>
             </div>
 
-            <div class="buildChangelog">
-              <p class="label">Device Changelog</p>
-              <pre>{{build.changelog}}</pre>
-            </div>
+            <div class="collapsible-body">
+              <p class="label">details</p>
+              <div class="buildinfo">
+                <div class="deviceprop">
+                  <p>Filename: {{ build.filename }}</p>
+                </div>
+                <div class="deviceprop">
+                  <p>Date: {{ build.datetime }}</p>
+                </div>
+                <div class="deviceprop">
+                  <p>Size: {{ build.size }}</p>
+                </div>
+                <div class="deviceprop">
+                  <p>MD5: {{ build.md5 }}</p>
+                </div>
+                <div class="deviceprop">
+                  <p>Version: {{ build.version }}</p>
+                </div>
+                <div class="deviceprop">
+                  <p>Downloads: {{ build.downloads }}</p>
+                </div>
+              </div>
 
-            <div class="buildbuttons">
-              <a
-                v-on:click="download(build.filename, device.codename)"
-                download
-                target="_blank"
-                class="btn"
-              >Download</a>
+              <div class="buildChangelog">
+                <p class="label">Device Changelog</p>
+                <pre>{{ build.changelog }}</pre>
+              </div>
+
+              <div class="buildbuttons">
+                <a
+                  v-on:click="download(build.filename, device.codename)"
+                  download
+                  target="_blank"
+                  class="btn"
+                  >Download</a
+                >
+              </div>
             </div>
-          </div>
-        </li>
-      </ul>
-      <Loading v-if="$store.state.buildLoader"/>
+          </li>
+        </ul>
+        <Loading v-if="$store.state.buildLoader" />
+      </div>
     </div>
   </div>
 </template>
 <script>
-import Loading from '../common/Loading.vue';
-import { generateDownloadURL } from '../../services/sourceforge';
+import Loading from "../common/Loading.vue";
+import { generateDownloadURL } from "../../services/sourceforge";
 
 export default {
-  name: 'CardBuilds',
+  name: "CardBuilds",
   components: {
-    Loading,
+    Loading
   },
   updated() {
     if (this.$route.params.filename) {
       this.$store.dispatch(
-        'getIndexOfExpandedBuild',
-        this.$route.params.filename,
+        "getIndexOfExpandedBuild",
+        this.$route.params.filename
       );
-      document.title = this.$route.params.filename || `Download ProjectSakura for ${this.$route.params.codename}`;
+      document.title =
+        this.$route.params.filename ||
+        `Download ProjectSakura for ${this.$route.params.codename}`;
     }
     this.openBuild(this.$store.state.expandedBuild);
-    this.$store.dispatch('getIndexOfExpandedBuild', '');
+    this.$store.dispatch("getIndexOfExpandedBuild", "");
   },
   methods: {
     setBuild(obj) {
-      const elems = document.querySelector('.collapsible-builds');
+      const elems = document.querySelector(".collapsible-builds");
       const instances = M.Collapsible.init(elems);
 
-      instances.options.onOpenEnd = () => this.$router.push({ name: 'filename', params: { filename: obj } });
+      instances.options.onOpenEnd = () =>
+        this.$router.push({ name: "filename", params: { filename: obj } });
 
-      instances.options.onCloseEnd = () => this.$router.replace({ name: 'filename', params: { filename: null } });
+      instances.options.onCloseEnd = () =>
+        this.$router.replace({ name: "filename", params: { filename: null } });
     },
     openBuild(index) {
       if (!isNaN(index) && index !== -1) {
-        const elems = document.querySelector('.collapsible-builds');
+        const elems = document.querySelector(".collapsible-builds");
         const instances = M.Collapsible.init(elems);
         instances.open(index);
       }
     },
     download(file, codename) {
-      M.toast({ html: 'Download Started' });
+      M.toast({ html: "Download Started" });
       location.href = generateDownloadURL(file, codename);
-    },
+    }
   },
   computed: {
     deviceBuilds() {
@@ -104,7 +131,7 @@ export default {
     },
     device() {
       return this.$store.state.device;
-    },
-  },
+    }
+  }
 };
 </script>
