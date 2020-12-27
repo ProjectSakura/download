27de39043430ad914b2efe58190570669b0e443f
ArunTeltia <teltia.arun18@gmail.com>
2020-12-27 20:18:09 +0530

    ad integration + buttons added

--- src/components/home/Credits.vue
+++ src/components/home/Credits.vue
@@ -9,6 +9,27 @@
       Website developed by
       <a href="https://github.com/andersonmendess">andersonmendess</a>
     </div>
+    <div class="container" style="width:100%">
+      <center>
+        <script
+          type="application/javascript"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
+        <!-- sakura-horizontal-unit-2 -->
+        <ins
+          class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-1277633863266032"
+          data-ad-slot="8931146882"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script type="application/javascript">
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </center>
+    </div>
   </div>
 </template>
 <script>
