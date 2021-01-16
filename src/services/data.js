e21813d8e823345cfb567fdf59d885fa02ecee6d
ArunTeltia <teltia.arun18@gmail.com>
2021-01-16 09:15:41 +0530

    Redmi Note 5 -> Redmi 5�

--- src/services/data.js
+++ src/services/data.js
@@ -131,7 +131,7 @@ const data = [
   },
   {
     brand: "Xiaomi",
-    name: "Redmi Note 5",
+    name: "Redmi 5",
     codename: "rosy",
     maintainer_name: "Shriiyansh",
     maintainer_xda: "null",
