cba208cf8fafd95159f80b4d50328a1a1023163a
LordShenron <shen.priyanshu@gmail.com>
2021-04-08 22:54:54 +0530

    Download: index missing devices and rearrange them
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/services/data.js
+++ src/services/data.js
@@ -20,12 +20,42 @@ const data = [
     active: true
   },
   {
+    brand: "Lenevo",
+    name: "Lenevo Z5S",
+    codename: "jd2019",
+    maintainer_name: "CakesTwix",
+    maintainer_xda: "null",
+    maintainer_country: "UKR",
+    xda_thread: "",
+    active: true
+  },
+  {
+    brand: "Meizu",
+    name: "Meizu 16TH",
+    codename: "m1882",
+    maintainer_name: "Abdibekov Dauren",
+    maintainer_xda: "null",
+    maintainer_country: "KAZ",
+    xda_thread: "",
+    active: true
+  },
+  {
+    brand: "Meizu",
+    name: "Meizu 16TH Plus",
+    codename: "m1892",
+    maintainer_name: "Abdibekov Dauren",
+    maintainer_xda: "null",
+    maintainer_country: "KAZ",
+    xda_thread: "",
+    active: true
+  },
+  {
     brand: "Moto",
     name: "Moto G5S+",
     codename: "sanders",
     maintainer_name: "ArmSM",
     maintainer_xda: "null",
-    maintainer_country: "ARM",
+    maintainer_country: "ALB",
     xda_thread: "",
     active: true
   },
@@ -43,9 +73,9 @@ const data = [
     brand: "OnePlus",
     name: "OnePlus 6",
     codename: "enchilada",
-    maintainer_name: "Zahidmgsk",
+    maintainer_name: "Eduardo",
     maintainer_xda: "null",
-    maintainer_country: "PAK",
+    maintainer_country: "US",
     xda_thread: "",
     active: true
   },
@@ -81,6 +111,16 @@ const data = [
   },
   {
     brand: "Samsung",
+    name: "Galaxy A20",
+    codename: "a20",
+    maintainer_name: "Chatur",
+    maintainer_xda: "null",
+    maintainer_country: "MAUR",
+    xda_thread: "",
+    active: true
+  },
+  {
+    brand: "Samsung",
     name: "Galaxy A30",
     codename: "a30",
     maintainer_name: "Haridhayal",
@@ -96,6 +136,16 @@ const data = [
     maintainer_name: "Haridhayal",
     maintainer_xda: "null",
     maintainer_country: "SL",
+    xda_thread: "",
+    active: false
+  },
+  {
+    brand: "Samsung",
+    name: "Galaxy J4 Plus",
+    codename: "j4primelte",
+    maintainer_name: "Gabriel",
+    maintainer_xda: "null",
+    maintainer_country: "BRA",
     xda_thread: "",
     active: false
   },
@@ -120,6 +170,36 @@ const data = [
     active: false
   },
   {
+    brand: "Samsung",
+    name: "Galaxy S8",
+    codename: "dreamlte",
+    maintainer_name: "Haridhayal",
+    maintainer_xda: "null",
+    maintainer_country: "SL",
+    xda_thread: "",
+    active: true
+  },
+  {
+    brand: "Samsung",
+    name: "Galaxy S8 Plus",
+    codename: "dream2lte",
+    maintainer_name: "Haridhayal",
+    maintainer_xda: "null",
+    maintainer_country: "SL",
+    xda_thread: "",
+    active: true
+  },
+  {
+    brand: "Samsung",
+    name: "Galaxy Note 8",
+    codename: "greatlte",
+    maintainer_name: "Haridhayal",
+    maintainer_xda: "null",
+    maintainer_country: "SL",
+    xda_thread: "",
+    active: true
+  },
+  {
     brand: "Xiaomi",
     name: "Poco X2",
     codename: "phoenix",
@@ -127,13 +207,13 @@ const data = [
     maintainer_xda: "null",
     maintainer_country: "IND",
     xda_thread: "",
-    active: true
+    active: false
   },
   {
     brand: "Xiaomi",
     name: "Poco X3",
     codename: "surya",
-    maintainer_name: "Agus",
+    maintainer_name: "Agus and ArmSM",
     maintainer_xda: "null",
     maintainer_country: "ID",
     xda_thread: "",
@@ -146,6 +226,36 @@ const data = [
     maintainer_name: "Roger W.",
     maintainer_xda: "null",
     maintainer_country: "BR",
+    xda_thread: "",
+    active: false
+  },
+  {
+    brand: "Xiaomi",
+    name: "Xiaomi Mi A2",
+    codename: "jasmine_sprout",
+    maintainer_name: "Marcos Almeida",
+    maintainer_xda: "null",
+    maintainer_country: "BR",
+    xda_thread: "",
+    active: false
+  },
+  {
+    brand: "Xiaomi",
+    name: "Xiaomi Mi 6X",
+    codename: "wayne",
+    maintainer_name: "Marcos Almeida",
+    maintainer_xda: "null",
+    maintainer_country: "BR",
+    xda_thread: "",
+    active: false
+  },
+  {
+    brand: "Xiaomi",
+    name: "Mi 10i 5G / 10T Lite",
+    codename: "gauguin",
+    maintainer_name: "Nitish x Prajwal",
+    maintainer_xda: "null",
+    maintainer_country: "IND",
     xda_thread: "",
     active: true
   },
@@ -161,13 +271,13 @@ const data = [
   },
   {
     brand: "Xiaomi",
-    name: "Redmi 6 Pro",
-    codename: "sakura",
-    maintainer_name: "Alex",
+    name: "Redmi K20",
+    codename: "davinci",
+    maintainer_name: "Gabriel",
     maintainer_xda: "null",
-    maintainer_country: "US",
+    maintainer_country: "BR",
     xda_thread: "",
-    active: true
+    active: false
   },
   {
     brand: "Xiaomi",
@@ -181,6 +291,16 @@ const data = [
   },
   {
     brand: "Xiaomi",
+    name: "Redmi 6 Pro",
+    codename: "sakura",
+    maintainer_name: "Alex",
+    maintainer_xda: "null",
+    maintainer_country: "US",
+    xda_thread: "",
+    active: false
+  },
+  {
+    brand: "Xiaomi",
     name: "Redmi Note 5 PRO",
     codename: "whyred",
     maintainer_name: "DarkStar",
@@ -191,21 +311,21 @@ const data = [
   },
   {
     brand: "Xiaomi",
-    name: "Redmi K20",
-    codename: "davinci",
-    maintainer_name: "Gabriel",
-    maintainer_xda: "null",
-    maintainer_country: "BR",
-    xda_thread: "",
-    active: true
-  },
-  {
-    brand: "Xiaomi",
     name: "Redmi Note 7 PRO",
     codename: "violet",
     maintainer_name: "SpicyDoughnut",
     maintainer_xda: "null",
     maintainer_country: "IND",
+    xda_thread: "",
+    active: true
+  },
+  {
+    brand: "Xiaomi",
+    name: "Redmi Note 8",
+    codename: "gingko",
+    maintainer_name: "Fernando Sanchez",
+    maintainer_xda: "null",
+    maintainer_country: "BR",
     xda_thread: "",
     active: true
   },
