0edd420fe2556e4a6ade22f61a938988c75c5f6d
Arun Teltia <43555219+ArunTeltia@users.noreply.github.com>
2021-05-18 18:15:07 +0530

    Added link in the promotion banners

--- src/components/common/Ninja.vue
+++ src/components/common/Ninja.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <div class="mobile_view">
-      <img :src="require('../../mobilead.png')" :width="width" alt="logo" />
-    </div>
-    <div class="pc_view">
-      <img :src="require('../../pcad.png')" :width="width" alt="logo" />
-    </div>
+    <a
+      href="https://www.codingninjas.com/courses/online-android-Development-kotlin?utm_source=projectsakura&utm_medium=mediabuy&utm_campaign=android-development
+"
+      target="_blank"
+    >
+      <div class="mobile_view">
+        <img :src="require('../../mobilead.png')" :width="width" alt="logo" />
+      </div>
+      <div class="pc_view">
+        <img :src="require('../../pcad.png')" :width="width" alt="logo" />
+      </div>
+    </a>
   </div>
 </template>
 <script>
