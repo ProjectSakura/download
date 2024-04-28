e3e1c183d4cf42e4aa133a974c7b240fa06ba010
ReveRTX <rev9rt@gmail.com>
2024-04-28 23:54:29 +0530

    Download: Update country flags
    Signed-off-by: ReveRTX <rev9rt@gmail.com>

--- src/components/common/Flag.vue
+++ src/components/common/Flag.vue
@@ -9,39 +9,39 @@ export default {
     flag() {
       const flags = {
         BR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-brazil_1f1e7-1f1f7.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-brazil_1f1e7-1f1f7.png",
         BRA:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-brazil_1f1e7-1f1f7.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-brazil_1f1e7-1f1f7.png",
         IND:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-india_1f1ee-1f1f3.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-india_1f1ee-1f1f3.png",
         US:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-united-states_1f1fa-1f1f8.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-united-states_1f1fa-1f1f8.png",
         UKR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-ukraine_1f1fa-1f1e6.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-ukraine_1f1fa-1f1e6.png",
         SL:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-sri-lanka_1f1f1-1f1f0.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-sri-lanka_1f1f1-1f1f0.png",
         RUS:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-russia_1f1f7-1f1fa.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-russia_1f1f7-1f1fa.png",
         ARM:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-armenia_1f1e6-1f1f2.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-armenia_1f1e6-1f1f2.png",
         ID:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/flag-indonesia_1f1ee-1f1e9.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-indonesia_1f1ee-1f1e9.png",
         PAK:
-          "https://flaglane.com/download/pakistani-flag/pakistani-flag-graphic.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-pakistan_1f1f5-1f1f0.png",
         MAUR:
-          "https://upload.wikimedia.org/wikipedia/commons/7/77/Flag_of_Mauritius.svg",
+          "https://em-content.zobj.net/source/twitter/376/flag-mauritius_1f1f2-1f1fa.png",
         KAZ:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/281/flag-kazakhstan_1f1f0-1f1ff.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-kazakhstan_1f1f0-1f1ff.png",
         ALB:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/281/flag-albania_1f1e6-1f1f1.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-albania_1f1e6-1f1f1.png",
         GER:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/281/flag-germany_1f1e9-1f1ea.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-germany_1f1e9-1f1ea.png",
         GBR:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/281/flag-united-kingdom_1f1ec-1f1e7.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-united-kingdom_1f1ec-1f1e7.png",
         OM:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/flag-oman_1f1f4-1f1f2.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-oman_1f1f4-1f1f2.png",
         VN:
-          "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/flag-vietnam_1f1fb-1f1f3.png",
+          "https://em-content.zobj.net/source/twitter/376/flag-vietnam_1f1fb-1f1f3.png",
       };
 
       if (flags[this.country]) {
